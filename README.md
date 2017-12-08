32d3067730b30b64e73e1f0bbe64450de90e0900
Nick Craig-Wood <nick@craig-wood.com>
2017-12-08 12:08:29 +0000

    Setup integration test server for rclone

---
+++ README.md
@@ -0,0 +1,72 @@
+# Rclone integration server #
+
+This describes the setup for the rclone integration server.  More details are probably needed!
+
+## How to install ##
+
+Install enough tools to build rclone
+
+    apt-get build-essentials
+
+Install the latest version of go from source
+
+Make sure go path is added to .profile
+
+    export GOPATH=$HOME/go
+
+set PATH so it includes go binary path in .profile
+
+    PATH="/usr/local/go/bin:$HOME/go/bin:$PATH"
+
+create an rclone user and have something like this on the crontab
+
+```
+SHELL=/bin/bash
+MAILTO=your@email-address.com
+
+0 6 * * * (cd ~/integration-test; date -Is; source ~/.profile; ./integration-test.sh) >> integration-test.log 2>&1
+```
+
+Make sure you have an rclone config with credentials for all the cloud providers.  `rclone listremotes` should look something like
+
+```
+TestAmazonCloudDrive:
+TestAzureBlob:
+TestB2:
+TestBox:
+TestCache:
+TestCryptDrive:
+TestCryptSwift:
+TestDrive:
+TestDropbox:
+TestFTP:
+TestGoogleCloudStorage:
+TestHubic:
+TestMega:
+TestOneDrive:
+TestOss:
+TestPcloud:
+TestQingStor:
+TestS3:
+TestSftp:
+TestSwift:
+TestWebdav:
+TestYandex:
+memstore:
+```
+
+
+## FTP ##
+
+Make a new user called testdata - this will be used to run the SFTP
+and FTP integration tests.  Make sure they have a very secure password
+and add it to the rclone config.
+
+Install pure-ftpd with the extra config file
+
+    # cat /etc/pure-ftpd/conf/Bind
+    127.0.0.1,21
+
+## SSH ##
+
+Make an ssh key for rclone user and install it in testdata user
---
+++ integration-test.sh
@@ -0,0 +1,62 @@
+ls
+#!/bin/bash
+
+set -e
+
+# run the rclone integration tests against all the remotes
+
+export GOPATH=~/go
+
+# checkout
+
+mkdir -p ${GOPATH}/src/github.com/ncw/rclone
+
+cd ${GOPATH}/src/github.com/ncw/rclone
+if [ -e ".git" ]; then
+    git checkout master
+    git pull
+else
+    git clone https://github.com/ncw/rclone.git .
+fi
+
+# run the tests
+
+make test || true
+
+# set TAG
+make vars | grep TAG >sourceme
+source sourceme
+rm sourceme
+
+# make paths
+outstore=memstore:pub-rclone-org
+outroot=integration-tests
+outpath=${outroot}/`date +'%Y-%m/%Y-%m-%d-%H%M%S'`
+www=https://pub.rclone.org/${outpath}
+out=${outstore}/${outpath}
+outtop=${outstore}/${outroot}
+
+# make summary
+rm -f summary
+touch summary
+echo >> summary "--------------------------------------------------------------" 
+echo >> summary "go test results - full results at ${www}/test-${TAG}.txt"
+echo >> summary "--------------------------------------------------------------" 
+grep >> summary FAIL test.log || echo >> summary "No test failures"
+echo >> summary
+echo >> summary "--------------------------------------------------------------" 
+echo >> summary "fs/test_all results - full results at ${www}/test_all-${TAG}.txt"
+echo >> summary "--------------------------------------------------------------" 
+grep >> summary -A1000 SUMMARY fs/test_all.log || echo >> summary "No SUMMARY found"
+
+mail nick@craig-wood.com -s "rclone integration tests" <summary
+
+# copy the results
+rclone copyto test.log ${out}/test-${TAG}.txt
+rclone copyto fs/test_all.log ${out}/test_all-${TAG}.txt
+rclone copyto summary ${out}/summary-${TAG}.txt
+
+# server side copy these to current
+rclone copyto ${out}/test-${TAG}.txt ${outtop}/current-test.txt
+rclone copyto ${out}/test_all-${TAG}.txt ${outtop}/current-test_all.txt
+rclone copyto ${out}/summary-${TAG}.txt ${outtop}/current-summary.txt
