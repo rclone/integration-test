76f810ce6ad11498595b24594d96035245c55d85
Nick Craig-Wood <nick@craig-wood.com>
2020-07-07 16:30:58 +0000

    Prune docker stuff so we don't run out of disk space

--- update-docker-images.sh
+++ update-docker-images.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 for image in $(docker images |grep -v REPOSITORY|awk '{print $1}'); do
-    echo "Updating $image"
-    docker pull $image
+   echo "Updating $image"
+   docker pull $image
 done
+
+echo "Prune everything"
+docker system prune --force
